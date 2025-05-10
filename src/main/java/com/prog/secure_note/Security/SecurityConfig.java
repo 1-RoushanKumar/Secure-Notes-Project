9051094e1acd95c52fb78a98a02c35ff4d3de4a1
Roushan Kumar <rk04393@gmail.com>
2025-05-10 13:10:34 +0530

    Added custom security config and then disabled the formlogin based authentication.

---
+++ src/main/java/com/prog/secure_note/Security/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.prog.secure_note.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    //copied from the SpringBootWebSecurityConfiguration.java class.
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()); // it says that all requests that are hitting the server need to be authenticated.
+//        http.formLogin(withDefaults()); // this is the default login form provided by Spring Security.
+        http.httpBasic(withDefaults()); // this is the default basic authentication provided by Spring Security.
+        return http.build();
+    }
+}
