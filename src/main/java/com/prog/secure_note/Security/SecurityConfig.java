6a3fb400e8de744f66695cc53f3f01c83c9d6ede
Roushan Kumar <rk04393@gmail.com>
2025-05-10 13:18:55 +0530

    making session stateless

--- src/main/java/com/prog/secure_note/Security/SecurityConfig.java
+++ src/main/java/com/prog/secure_note/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,6 +18,8 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated()); // it says that all requests that are hitting the server need to be authenticated.
 //        http.formLogin(withDefaults()); // this is the default login form provided by Spring Security.
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //here i made the session stateless, so that the server does not create a session for the user.
         http.httpBasic(withDefaults()); // this is the default basic authentication provided by Spring Security.
         return http.build();
     }
