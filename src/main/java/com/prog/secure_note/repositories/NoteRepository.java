f0e0935deb83f290fb910411c5aa4776576c24e0
Roushan Kumar <rk04393@gmail.com>
2025-05-11 16:19:39 +0530

    so i created a repository, then service class.

---
+++ src/main/java/com/prog/secure_note/repositories/NoteRepository.java
@@ -0,0 +1,12 @@
+package com.prog.secure_note.repositories;
+
+import com.prog.secure_note.model.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findByOwnerUsername(String username);
+}
---
+++ src/main/java/com/prog/secure_note/service/NoteService.java
@@ -0,0 +1,17 @@
+package com.prog.secure_note.service;
+
+import com.prog.secure_note.model.Note;
+
+import java.util.List;
+
+public interface NoteService {
+    Note createNoteForUser(String username, String content);
+
+    Note updateNoteForUser(Long noteId, String content, String username);
+
+    void deleteNoteForUser(Long noteId, String username);
+
+    List<Note> getNotesForUser(String username);
+}
+
+//here i simply written a simple crud operation.
