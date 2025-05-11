804880f6909b9d94f0d5593a1dbd9c8f867fc3ec
Roushan Kumar <rk04393@gmail.com>
2025-05-11 18:36:36 +0530

    added In Memory Authentication.

--- src/main/java/com/prog/secure_note/security/SecurityConfig.java
+++ src/main/java/com/prog/secure_note/security/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.prog.secure_note.security;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,5 +22,30 @@ public class SecurityConfig {
         http.httpBasic(withDefaults());
         http.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+    //In memory user details manager for authentication.
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        if (!manager.userExists("user1")) {
+            manager.createUser(
+                    User.withUsername("user1")
+                            .password("{noop}password1")
+                            .roles("USER")
+                            .build()
+            );
+        }
+        if (!manager.userExists("admin")) {
+            manager.createUser(
+                    User.withUsername("admin")
+                            .password("{noop}adminPass")
+                            .roles("ADMIN")
+                            .build()
+            );
+        }
+
+        return manager;
     }
 }
