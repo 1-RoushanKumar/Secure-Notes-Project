6f820ae826ee7101cc7f970777970b5033e21c55
Roushan Kumar <rk04393@gmail.com>
2025-05-12 11:36:01 +0530

    Adding AdminController.

---
+++ src/main/java/com/prog/secure_note/service/UserService.java
@@ -0,0 +1,15 @@
+package com.prog.secure_note.service;
+
+import com.prog.secure_note.model.User;
+import com.prog.secure_note.model.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+    void updateUserRole(Long userId, String roleName);
+
+    List<User> getAllUsers();
+
+    UserDTO getUserById(Long id);
+}
+
