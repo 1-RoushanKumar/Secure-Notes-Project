69aa46aaab224bfb61c9b5f76e7544a001b10e65
Roushan Kumar <rk04393@gmail.com>
2025-05-12 14:55:01 +0530

    i deleted the CustomLoggingFilter.java and RequestValidationFilter.java filter because that is i added it only for learning purpose.

--- src/main/java/com/prog/secure_note/security/SecurityConfig.java
+++ src/main/java/com/prog/secure_note/security/SecurityConfig.java
@@ -8,13 +8,11 @@ import com.prog.secure_note.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.time.LocalDate;
 
@@ -22,7 +20,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-//@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true) //added method level security.
 public class SecurityConfig {
 
     @Bean
@@ -33,8 +30,6 @@ public class SecurityConfig {
                         .anyRequest().authenticated());
         http.formLogin(withDefaults());
         http.csrf(csrf -> csrf.disable());
-        http.addFilterBefore(new CustomLoggingFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterAfter(new RequestValidationFilter(), CustomLoggingFilter.class);
         http.httpBasic(withDefaults());
         return http.build();
     }
